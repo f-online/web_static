95df64c759bced2669bfda0323add13b680d19e8
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-12-13 21:59:54 +0100

    Project setup
    - Installed ESLint
    - Removed unused boilerplate
    - Settings for VSCode

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,12 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.f-online.at",
-    title: "F-Online",
+    siteUrl: 'https://www.f-online.at',
+    title: 'F-Online',
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
   ],
 };
