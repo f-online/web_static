5bf6c674d5352503e5e7413244e01bd3b603d829
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-12-13 22:15:16 +0100

    Add current pages and base components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Footer from './Footer';
+import Nav from './Nav';
 
 export default function Layout({ children }) {
   return (
     <>
+      <Nav />
+
       {children}
+
+      <Footer />
     </>
   );
 }
