548eadab47af93ed4e340a448726965928530293
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-12-18 00:30:47 +0100

    Add section header component

---
+++ src/components/SectionHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SectionHeader({ title, subtitle }) {
+  return (
+    <div className="mb-10">
+      <h2 className="text-4xl text-center font-bold mb-2">{title}</h2>
+      <div className="w-10 h-0.5 bg-fonline-500 mx-auto mb-3" />
+      <h4 className="text-center text-gray-500 max-w-sm mx-auto">{subtitle}</h4>
+    </div>
+  );
+}
