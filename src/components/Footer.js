9aff3d337171c8c6c96fdee4ca2554b49c1412a4
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-12-18 00:38:55 +0100

    Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,17 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="px-40 py-10 text-white bg-fonline-500 flex flex-col text-center md:text-left md:flex-row md:justify-between">
+      <div>
+        Made with ♥ by F-Online.
+      </div>
+      <div>
+        <Link to="/impressum" className="pr-4">Impressum</Link>
+        <Link to="/toc">Terms and conditions</Link>
+      </div>
+
+    </footer>
+  );
 }
