f1451c895408ebf686a29c3ca24cfb90bb6ab52c
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-12-21 22:17:58 +0100

    Add wide screen support, customizable padding y

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default function Section({ className, children }) {
+export default function Section({
+  className, children, py = 20, containerClassName,
+}) {
   return (
-    <section className={`px-10 md:px-20 lg:px-40 py-20 ${className}`}>
-      {children}
+    <section className={`px-10 md:px-20 lg:px-40 py-${py} ${className}`}>
+      <div className={`max-w-screen-2xl m-auto ${containerClassName}`}>
+        {children}
+      </div>
     </section>
   );
 }
