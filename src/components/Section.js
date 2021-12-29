1df56861481c7b6fff5e4fdcd754b6c9c956e377
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-12-29 12:31:13 +0100

    Fix padding for sections, add auth form link, add placeholder image

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default function Section({
-  className, children, py = 20, containerClassName,
+  className, children, containerClassName,
 }) {
   return (
-    <section className={`px-10 md:px-20 lg:px-40 py-${py} ${className}`}>
-      <div className={`max-w-screen-2xl m-auto ${containerClassName}`}>
+    <section className={`px-10 md:px-20 lg:px-40 py-20 ${className}`}>
+      <div className={`container m-auto ${containerClassName}`}>
         {children}
       </div>
     </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,22 +8,21 @@ export default function IndexPage() {
   return (
     <>
       <div className="bg-gradient-to-r from-fonline-600 via-fonline-700 to-fonline-500 ">
-        <Section containerClassName="flex flex-col lg:flex-row" py={10}>
-          <div className="w-full p-10 text-white text-4xl font-bold order-2 lg:order-1">
-            <h1>
-              Some random marketing
-            </h1>
+        <Section containerClassName="flex flex-col lg:flex-row">
+          <div className="w-full pr-10 text-white text-left font-bold order-2 lg:order-1">
+            <img src="https://d33wubrfki0l68.cloudfront.net/a33b57a4be108ccdf2a32e3702272ed5c4904952/7e28f/img/bg-mac.png" />
           </div>
           <div className="bg-white p-6 rounded text-center order-1 lg:order-2">
             <h1 className="text-4xl font-semibold">Kostenlos für den Führerschein lernen</h1>
             <h2 className="text-gray-500 mt-3">mit den offiziellen Fragen (Stand: 14.12.2021) für die österreichische Fahrprüfung. Wir sind und bleiben gratis!</h2>
 
-            <form className="mt-10 mb-8 text-left">
+            <form action="//app.f-online.at/auth" method="post" className="mt-10 mb-8 text-left">
               <div className="mb-5">
                 <label htmlFor="mail" className="font-semibold">E-Mail:</label>
                 <div className="mt-1">
                   <input
                     type="email"
+                    name="username"
                     placeholder="max@mustermann.de"
                     id="mail"
                     className="w-full border-gray-500 rounded py-2 focus:border-fonline-500 focus:ring-fonline-500"
@@ -36,6 +35,7 @@ export default function IndexPage() {
                 <div className="mt-1">
                   <input
                     type="password"
+                    name="password"
                     id="password"
                     className="w-full border-gray-500 rounded py-2 focus:border-fonline-500 focus:ring-fonline-500"
                   />
@@ -62,8 +62,11 @@ export default function IndexPage() {
           </div>
         </Section>
       </div>
+
       <Features className="bg-fonline-50" />
+
       <Team />
+
       <FAQ className="bg-fonline-50" limit={5} />
     </>
   );
