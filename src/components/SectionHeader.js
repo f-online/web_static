0e409e144efc469775b7fe8d09f7c122848f2246
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2021-12-30 20:10:01 +0100

    Add aria hidden to element not necessary for screen readers

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -4,7 +4,7 @@ export default function SectionHeader({ title, subtitle }) {
   return (
     <div className="mb-10">
       <h2 className="text-4xl text-center font-bold mb-2">{title}</h2>
-      <div className="w-10 h-0.5 bg-fonline-500 mx-auto mb-3" />
+      <div aria-hidden className="w-10 h-0.5 bg-fonline-500 mx-auto mb-3" />
       <h4 className="text-center text-gray-500 max-w-md mx-auto">{subtitle}</h4>
     </div>
   );
