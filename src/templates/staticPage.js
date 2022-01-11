6f429e6070f561a84aea7afab98ccfb55377fef3
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2022-01-11 23:10:40 +0100

    Use static pages of sanity to generate pages

--- src/templates/staticPage.js
+++ src/templates/staticPage.js
@@ -2,33 +2,59 @@ import BlockContent from '@sanity/block-content-to-react';
 import { graphql } from 'gatsby';
 import React from 'react';
 import Section from '../components/Section';
+import Quote from '../components/Quote';
 import SectionHeader from '../components/SectionHeader';
+import Team from '../components/Team';
 
 export default function StaticPage({ data: { staticPage } }) {
   return (
     <>
       {staticPage.sections.map((section, index) => {
+        if (section._type === 'section') {
         // Stripe background
-        let cssClass = '';
-        if (index === 0 || index % 2 === 0) {
-          cssClass = 'bg-fonline-50';
+          let cssClass = '';
+          if (index === 0 || index % 2 === 0) {
+            cssClass = 'bg-fonline-50';
+          }
+
+          // special cases
+          if ((staticPage.slug.current === 'impressum' && index === 0) || staticPage.slug.current === 'kontakt') {
+            cssClass += ' text-center';
+          }
+
+          return (
+            <Section className={cssClass}>
+              <SectionHeader
+                title={section.title}
+                subtitle={section.subtitle}
+              />
+
+              <BlockContent blocks={section._rawContent} ignoreUnknownTypes />
+            </Section>
+          );
         }
 
-        // special impressum case
-        if (staticPage.slug.current === 'impressum') {
-          cssClass += ' text-center';
+        if (section._type === 'quote') {
+          return (
+            <Quote qoute={section.quote} author={section.author} year={section.year} />
+          );
         }
 
-        return (
-          <Section className={cssClass}>
-            <SectionHeader
-              title={section.title}
-              subtitle={section.subtitle}
-            />
+        if (section._type === 'teamObject') {
+          return (
+            <Section>
+              <SectionHeader
+                title={section.title}
+                subtitle={section.subTitle}
+              />
 
-            <BlockContent blocks={section._rawContent} ignoreUnknownTypes />
-          </Section>
-        );
+              <Team />
+            </Section>
+          );
+        }
+
+        // Fallback for not known elements
+        return <></>;
       })}
     </>
   );
@@ -43,9 +69,23 @@ export const query = graphql`
         current
       }
       sections {
-        title
-        subTitle
-        _rawContent
+        ... on SanityQuote {
+          _type
+          author
+          quote
+          year(formatString: "YYYY")
+        }
+        ... on SanitySection {
+          _type
+          title
+          subTitle
+          _rawContent
+        }
+        ... on SanityTeamObject {
+          _type
+          title
+          subTitle
+        }
       }
     }
   }
