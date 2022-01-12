f996deae2da7b06246cd8511e17081d2de343c81
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2022-01-12 22:49:54 +0100

    Add basic 404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,16 @@
 import * as React from 'react';
+import ButtonLink from '../components/ButtonLink';
+import Section from '../components/Section';
+import SectionHeader from '../components/SectionHeader';
 
 export default function NotFoundPage() {
-  return <>404 not found</>;
+  return (
+    <Section>
+      <SectionHeader title="Falsch abgebogen?" subtitle="Leider können wir nicht bei alle Fragen helfen. Aber vielleicht verbirgt sich hinter dem folgenden Link die Lösung?" />
+
+      <div className="text-center">
+        <ButtonLink to="/" text="Hier entlang" />
+      </div>
+    </Section>
+  );
 }
