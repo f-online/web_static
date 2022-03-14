1b6bb4c1c336b07a8892e0455d4a5609a25f731e
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2022-03-14 22:07:27 +0100

    Simplify team style, remove mail, change styles for safari

--- src/components/Team.js
+++ src/components/Team.js
@@ -27,13 +27,7 @@ export default function Team() {
       {team.nodes.map((teammember) => (
         <div className="flex flex-col align-items-center text-center p-5" key={teammember.id}>
           <div className="mx-auto mb-5 relative">
-            <GatsbyImage image={teammember.image.asset.gatsbyImageData} alt={teammember.name} className="h-40 w-40 rounded-full" />
-            <a href={`mailto:${teammember.mail}`} className="absolute h-6 w-6 right-0 bottom-2 p-5 bg-fonline-500 hover:bg-fonline-700 rounded-l-xl rounded-t-xl">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-white absolute top-2 left-[0.5rem]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-              </svg>
-            </a>
-
+            <GatsbyImage image={teammember.image.asset.gatsbyImageData} alt={teammember.name} className="h-40 w-40 rounded-xl" />
           </div>
           <p className="text-xl p-0">{teammember.name}</p>
           <p className="text-gray-500 p-0">{teammember.position}</p>
