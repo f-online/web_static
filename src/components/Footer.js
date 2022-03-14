f98a9a306fe26f450d422f35b1e30bf61e738cd0
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2022-03-14 15:32:53 +0100

    Add footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,33 @@
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import React from 'react';
 
 export default function Footer() {
+  const { footerLinksNodes } = useStaticQuery(graphql`
+    query {
+      footerLinksNodes: allSanityCountry(filter: {countryCode: {eq: "at"}}) {
+        nodes {
+          staticPageFooterLinks {
+            title
+            slug {
+              current
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const footerLinks = footerLinksNodes.nodes[0].staticPageFooterLinks;
+
   return (
     <footer className="px-40 py-10 text-white bg-fonline-500 flex flex-col text-center md:text-left md:flex-row md:justify-between">
       <div>
         Made with ♥ by F-Online.
       </div>
       <div>
-        <Link to="/impressum" className="pr-4">Impressum</Link>
-        <Link to="/agb">AGB</Link>
+        {footerLinks.map((footerLink) => (
+          <Link to={footerLink.slug.current} className="pr-4">{footerLink.title}</Link>
+        ))}
       </div>
 
     </footer>
