54f6182a4b7c85fddd2b6b33681ae6dd6a579094
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2022-03-27 20:55:49 +0200

    Mobile login design fixes
    Use more space for mobile devices

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,15 +7,15 @@ import Waves from './Waves';
 export default function Login({ title, subTitle, screenshot }) {
   return (
     <div className="bg-gradient-to-r from-fonline-600 via-fonline-700 to-fonline-500 ">
-      <Section containerClassName="flex flex-col lg:flex-row">
+      <Section containerClassName="flex flex-col lg:flex-row" className="px-5 py-5">
         <div className="w-full pr-10 text-white text-left font-bold order-2 lg:order-1">
           <GatsbyImage image={screenshot} alt="Screenshot" />
         </div>
         <div className="bg-white p-6 rounded text-center order-1 lg:order-2">
-          <h1 className="text-4xl font-semibold">{title}</h1>
+          <h1 className="text-3xl md:text-4xl font-semibold">{title}</h1>
           <h2 className="text-gray-500 mt-3">{subTitle}</h2>
 
-          <form action="//app.f-online.at/auth" method="post" className="mt-10 mb-8 text-left">
+          <form action="//app.f-online.at/auth" method="post" className="mt-5 md:mt-10 mb-8 text-left">
             <div className="mb-5">
               <label htmlFor="mail" className="font-semibold">E-Mail:</label>
               <div className="mt-1">
@@ -41,10 +41,10 @@ export default function Login({ title, subTitle, screenshot }) {
               </div>
             </div>
 
-            <div className="flex">
+            <div className="flex flex-col md:flex-row">
               <input type="submit" value="Anmelden" className="w-full cursor-pointer py-4 px-10 bg-fonline-500 hover:bg-fonline-700 text-white font-bold rounded-l-xl rounded-t-xl transition duration-200 no-underline hover:text-white" />
               <a
-                className="bg-teal-600 text-white hover:bg-teal-800 hover:text-white ml-2 py-4 px-10 inline-flex items-center rounded-r-xl rounded-t-xl no-underline"
+                className="bg-teal-600 text-white hover:bg-teal-800 hover:text-white md:ml-2 py-4 px-10 inline-flex items-center rounded-l-xl md:rounded-tl-xl md:rounded-bl-none md:rounded-r-xl rounded-t-xl no-underline mt-4 md:mt-0"
                 href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=RL9PYUBA3MJ2S&source=url"
               >
                 <BiDonateHeart className="mr-2" />
