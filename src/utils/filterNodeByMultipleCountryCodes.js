34e15fd7e5c935793e11a3aa1d06877b1cc7ec13
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2022-04-01 14:43:59 +0200

    Remove debug code

--- src/utils/filterNodeByMultipleCountryCodes.js
+++ src/utils/filterNodeByMultipleCountryCodes.js
@@ -2,12 +2,8 @@ export default function filterNodeByMultipleCountryCodes(nodes, countryCode) {
   const nodesInCountry = [];
 
   nodes.forEach((node) => {
-    console.log(node);
     node.countries.forEach((nodeCountry) => {
-      console.log(nodeCountry.countryCode);
-      console.log(countryCode);
       if (nodeCountry.countryCode === countryCode) {
-        console.log('here');
         nodesInCountry.push(node);
       }
     });
