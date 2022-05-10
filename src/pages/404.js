824e9ecff32561f69332c5457e3ab52d824095c2
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2022-05-10 23:19:57 +0200

    Use 'at' layout for not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,18 +3,19 @@ import ButtonLink from '../components/ButtonLink';
 import Section from '../components/Section';
 import SectionHeader from '../components/SectionHeader';
 import SEO from '../components/SEO';
+import Layout from '../components/Layout';
 
 export default function NotFoundPage() {
   return (
-    <>
+    <Layout countryCode="at">
       <SEO title="Seite nicht gefunden" />
       <Section>
         <SectionHeader title="Falsch abgebogen?" subtitle="Leider können wir nicht bei alle Fragen helfen. Aber vielleicht verbirgt sich hinter dem folgenden Link die Lösung?" />
 
         <div className="text-center">
-          <ButtonLink to="/" text="Hier entlang" />
+          <ButtonLink to="/at/" text="Hier entlang" />
         </div>
       </Section>
-    </>
+    </Layout>
   );
 }
