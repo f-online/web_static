25dc140f26b43905523b979a665bdfd58fd38056
Wolfgang Gassler <wolfgang@gassler.org>
2022-07-14 16:13:22 +0200

    school listing design changes acc. to tim

--- src/templates/drivingSchools/perCountry.js
+++ src/templates/drivingSchools/perCountry.js
@@ -46,28 +46,30 @@ export default function drivingSchoolsPerCountryPage({ pageContext, data: { driv
                 <Link to={`/${pageContext.countryCode}/fahrschulen/${replaceUmlauts(region.regionName)}`} className="no-underline">{region.regionName}</Link>
               </h3>
               <div className="w-10 h-0.5 bg-fonline-500 mx-auto mb-3" />
-              <h4 className="text-center text-gray-500 max-w-md mx-auto mb-5">{`${region.drivingSchools.length} Fahrschulen sind in ${region.regionName} zu finden`}</h4>
-              <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10">
+              <h4 className="text-center text-gray-500 max-w-md mx-auto mb-5">{`${region.drivingSchools.length} Fahrschulen in ${region.regionName} empfehlen F-Online`}</h4>
+              <div className="flex flex-row flex-wrap justify-center items-center">
                 {region.drivingSchools.map((drivingSchool) => (
-                  <div
-                    className="p-5 rounded-lg text-center border-b-4 hover:bg-white border-transparent hover:border-fonline-500 hover:shadow-2xl transition-shadow duration-300"
-                    key={drivingSchool.id}
-                  >
-                    <div className="flex items-center justify-center h-40 mb-3">
-                      <GatsbyImage
-                        image={drivingSchool.logo.asset.gatsbyImageData}
-                        alt={drivingSchool.name}
-                      />
-                    </div>
-                    <div>
-                      <strong>{drivingSchool.name}</strong>
-                      <br />
-                      {drivingSchool.street}
-                      <br />
-                      {`${drivingSchool.zip} ${drivingSchool.city}`}
-                      <br />
-                      <a href={drivingSchool.url}>{drivingSchool.url}</a>
-                      <br />
+                  <div className="w-full sm:w-1/2 lg:w-1/3 xl:w-1/4">
+                    <div
+                      className="m-6 p-5 rounded-lg text-center border-b-4 bg-white border-transparent hover:border-fonline-500 hover:shadow-2xl transition-shadow duration-300"
+                      key={drivingSchool.id}
+                    >
+                      <div className="flex items-center justify-center h-40 mb-3">
+                        <GatsbyImage
+                          image={drivingSchool.logo.asset.gatsbyImageData}
+                          alt={drivingSchool.name}
+                        />
+                      </div>
+                      <div>
+                        <strong>{drivingSchool.name}</strong>
+                        <br />
+                        {drivingSchool.street}
+                        <br />
+                        {`${drivingSchool.zip} ${drivingSchool.city}`}
+                        <br />
+                        <a href={drivingSchool.url}>{drivingSchool.url}</a>
+                        <br />
+                      </div>
                     </div>
                   </div>
                 ))}
