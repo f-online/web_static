7190c8f64b14e75e4830ef0cc45667ea38789c11
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2024-03-29 21:41:01 +0100

    Add banner

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Footer from './Footer';
 import '../styles/tailwind.css';
 import Nav from './Nav';
@@ -6,6 +7,13 @@ import Nav from './Nav';
 export default function Layout({ children, countryCode }) {
   return (
     <div className="flex flex-col h-screen justify-between">
+      {/* Banner for updates */}
+      <section className="bg-emerald-500 text-white text-center py-4">
+        <strong className="text-white">Neues Fragenupdate!</strong>
+        {' '}
+        <span className="hidden sm:inline">Updates des Fachverbandes vom 29.03.24 -</span>
+        <Link to="/at/fragenupdate-29-03-2024" className=" text-white px-2 py-1 underline hover:text-white hover:underline rounded-l-xl rounded-t-xl">Mehr erfahren</Link>
+      </section>
       <Nav countryCode={countryCode} />
 
       <main className="mb-auto">
