15ae6b577dd95e1820ddbb5ebff134b2888d5120
Mischa Helfenstein <helfenstein.mischa@gmail.com>
2024-04-09 21:16:24 +0200

    Small SEO adjustments

--- src/templates/question.js
+++ src/templates/question.js
@@ -7,7 +7,10 @@ import Question from '../components/Question';
 export default function StaticPage({ pageContext: { question } }) {
   return (
     <Layout countryCode="at">
-      <SEO title={question.question} />
+      <SEO
+        title={`Frage ${question.qst_id}`}
+        description={`Frage ${question.qst_id}: ${question.txt_text}`}
+      />
 
       <Section className="container mx-auto pt-10">
         <Question question={question} />
